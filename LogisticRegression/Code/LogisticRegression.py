436c29621af8578fa24ac186f2e8735f3e7b7a22
Saurav <sorghosh@gmail.com>
2017-04-15 17:05:25 +0530

    Revert "Logistic_Regression"
    This reverts commit 948cbb5fc4b8854f3a4281964b6fbd8b34d62016.

--- LogisticRegression/Code/LogisticRegression.py
+++ LogisticRegression/Code/LogisticRegression.py
@@ -8,9 +8,9 @@ class logistic_regression :
         self.data.insert(0,"Ones",1)
         self.shape  = data.shape[1]
         
-    def sigmoid(self,z):
+    def sigmoid(self,x):
         ## this function converts the predictions into probablity
-        return 1/(1 + np.exp(-z))
+        return 1/(1 + np.exp(-x))
         
     def cost(self,theta,x,y,):
         x      = np.matrix(x)
@@ -18,7 +18,7 @@ class logistic_regression :
         theta  = np.matrix(theta)
         first  = np.multiply(-y, np.log(self.sigmoid(x * theta.T)))
         second = np.multiply((1-y),np.log(1- self.sigmoid(x* theta.T)))
-        return np.sum(first - second)/(len(x))
+        return np.sum(first - second)/len(x)
         
     def gradient(self,theta,x,y):
         x          = np.matrix(x)
@@ -29,31 +29,21 @@ class logistic_regression :
         error      = self.sigmoid(x * theta.T) - y
         for i in range(parameters):
             term = np.multiply(error , x[:,i])
-            grad[i] = np.sum(term)/(len(x))
+            grad[i] = np.sum(term)/len(x)
         return grad
     
-    def optimize(self,x,y):
-#        x      = np.array(self.data.iloc[:,:self.shape-1])
-#        y      = np.array(self.data.iloc[:,self.shape-1:]) 
+    def optimize(self):
+        x      = np.array(self.data.iloc[:,:self.shape-1])
+        y      = np.array(self.data.iloc[:,self.shape-1:]) 
         theta  = np.zeros(self.shape-1)   
         result = opt.fmin_tnc(func = self.cost, x0 = theta, fprime = self.gradient, args = (x,y))
 
         return result , self.cost(result[0],x,y)
-        
-    def predict(self):
-        x = np.array(data.iloc[:,0:self.shape-1])
-        y = np.array(data.iloc[:,self.shape-1: self.shape])
-        result ,cost = self.optimize(x,y)
-        predict = self.sigmoid(np.matrix(x) * np.matrix(result[0]).T)
-        return pd.DataFrame(predict)
 
-
-data = pd.read_csv(r"C:\Users\sauravghosh\Desktop\Machine_Learning\Algo\LogisticRegression\Data\data.txt",header = None)
+data = pd.read_csv(r"C:\Users\sauravghosh\Desktop\Machine_Learning\Algo\LogisticRegression\Data\data.txt")
 data.columns = ['Exam 1', 'Exam 2', 'Admitted']
 obj = logistic_regression(data)
-predict = obj.predict()
-
-
+result , cost = obj.optimize()
 
 
 
